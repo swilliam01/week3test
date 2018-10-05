5ee06b5e77e0814eacd0c0f7589ec4dcdd65a4b4
swilliam01 <sehrishwilliam@gmail.com>
2018-10-05 17:20:17 -0400

    i have created it

--- src/me/sherry/week3Challenge.java
+++ src/me/sherry/week3Challenge.java
@@ -13,22 +13,38 @@ public class week3Challenge {
         System.out.println("Enter a number : ");
 
         int x = (keyboard.nextInt());
-        int multiplesOf5;
-        int multipleOf7;
+        int counter = x + 1;
 
-        for ( int counter = x; counter>=10; counter--)
-        {
-            if (counter / 5 ==0)
-            System.out.println(counter);
-            System.out.println(counter-2);
+            while (counter > 1)
+            { counter = counter -1;
+            }
+            if  (counter % 5 ==0) {
+                System.out.println(counter);
+            }
+            else if (counter % 7 == 0)
+                { System.out.println(counter);
+                }
+
+        { System.out.println(counter);
+        }
+        String input = ("");
+       String yesNo = ("");
+
+         System.out.println("Would you like to enter another number (y/n) ? ");
+            System.out.println();
+            yesNo = keyboard.nextLine();
+
+            System.out.println("Enter a number :");
+
+            System.out.println("Thank you for playing !");
 
 
         }
+
 
 
     }
 
-        }
 
 
 
